975e0d45e9b3ee893d52bfb0ca10ace7a2452a94
VienNguyen <congvien.01@gmail.com>
2022-11-27 22:50:38 +0700

    xong feedback

--- src/main/java/com/websiteshop/controller/FeedbackController.java
+++ src/main/java/com/websiteshop/controller/FeedbackController.java
@@ -8,33 +8,38 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import com.websiteshop.entity.Feedback;
 import com.websiteshop.model.FeedbackDto;
+import com.websiteshop.service.AccountService;
 import com.websiteshop.service.FeedbackService;
 @Controller
 public class FeedbackController {
 	@Autowired
 	FeedbackService feedbackService;
 	
-	@RequestMapping("user/feedback")
-	public String add(Model model) {
-		FeedbackDto dto = new FeedbackDto();
-		model.addAttribute("feedback", dto);
+	@Autowired
+	AccountService accountService;
+	
+	Feedback entity = new Feedback();
+	
+	@RequestMapping("user/feedback/{username}")
+	public String add(Model model, @PathVariable("username")String username) {
+		entity.setUsername(username);
 		return "user/feedback";
 	}
+	
 
 	@PostMapping("user/feedback/save")
-	public ModelAndView save(ModelMap model,
-			@ModelAttribute("feedback") FeedbackDto dto, BindingResult result) {
+	public String save(ModelMap model,
+			@ModelAttribute("feedback")FeedbackDto dto, BindingResult result) {
 		
-		Feedback entity = new Feedback();
 		BeanUtils.copyProperties(dto, entity);
 		
 		feedbackService.save(entity);
 		model.addAttribute("message", "Cảm ơn bạn đã góp ý!");
-		return new ModelAndView("forward:/user/feedback", model);
+		return "user/feedback";
 	}
 }
--- src/main/java/com/websiteshop/model/FeedbackDto.java
+++ src/main/java/com/websiteshop/model/FeedbackDto.java
@@ -1,9 +1,6 @@
 package com.websiteshop.model;
 
 import java.io.Serializable;
-
-import com.websiteshop.entity.Account;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,5 +12,4 @@ public class FeedbackDto  implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private Long feedbackId;
 	private String description;
-	private String username;
 }
