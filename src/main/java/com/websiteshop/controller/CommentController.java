455e410383ba13713eb947c9378c49b51b9de350
VienNguyen <congvien.01@gmail.com>
2022-12-22 21:02:38 +0700

    new Product comment function

--- src/main/java/com/websiteshop/controller/CommentController.java
+++ src/main/java/com/websiteshop/controller/CommentController.java
@@ -1,17 +1,17 @@
 package com.websiteshop.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
 import com.websiteshop.entity.Comment;
 import com.websiteshop.model.CommentDto;
 import com.websiteshop.service.CommentService;
@@ -22,33 +22,32 @@ public class CommentController {
 	@Autowired
 	CommentService commentService;
 
-	@GetMapping("add")
-	public String addComment(Model model) {
+	@GetMapping("add/{productId}")
+	public String addComment(Model model, @PathVariable("productId") Long productId) {
 		CommentDto dto = new CommentDto();
-		model.addAttribute("comment", dto);
-		return "product/comment";
-	}
-
-	@GetMapping("reset")
-	public String reset(Model model) {
-		CommentDto p = new CommentDto();
-		p.setDescription("");
-		model.addAttribute("comment", p);
+		dto.setProductId(productId);
+		model.addAttribute("Comment", dto);
 		return "product/comment";
 	}
 
 	@PostMapping("saveOrUpdate")
-	public String saveOrUpdate(ModelMap model,
-			@ModelAttribute("comment") CommentDto dto, BindingResult result) {
+	public String saveOrUpdate(Model model, HttpServletRequest request,
+			@ModelAttribute("Comment") CommentDto dto, BindingResult result) {
 
-		if (result.hasErrors()) {
+		try {
+			if (result.hasErrors()) {
+				return "product/comment";
+			}
+			String username = request.getRemoteUser();
+			dto.setUsername(username);
+			Comment entity = new Comment();
+			BeanUtils.copyProperties(dto, entity);
+			commentService.save(entity);
+			model.addAttribute("message", "Cảm ơn bạn đã đánh giá sản phẩm.");
+			return "product/comment";
+		} catch (Exception e) {
+			model.addAttribute("message", "Bạn cần phải đăng nhập trước khi bình luận.");
 			return "product/comment";
 		}
-		Comment entity = new Comment();
-		BeanUtils.copyProperties(dto, entity);
-
-		commentService.save(entity);
-		model.addAttribute("message", "Cảm ơn bạn đã đánh giá sản phẩm.");
-		return "product/comment";
 	}
 }
